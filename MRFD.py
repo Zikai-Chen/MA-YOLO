84f3e633a22047a21cd887966e817e892cbc91f2
ZikaiChen <149089196+Zikai-Chen@users.noreply.github.com>
2024-11-05 13:14:48 +0800

    Update MRFD.py

--- MRFD.py
+++ MRFD.py
@@ -47,32 +47,6 @@ class SPDConv(nn.Module):
     def forward(self, x):
         return torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1)
 
-class MRFD(nn.Module):           # SPDConv + 3*3Conv + AvgMaxConv(论文一)
-    def __init__(self, c1, c2):
-        super().__init__()
-        # 3059708 parameters, 3059692 gradients, 8.3 GFLOPs
-        self.spd = SPDConv(c1, 4 * c1)
-        self.cbs = CBS(4 * c1, c1)
 
-        self.cbs1 = CBS(c1, c1, 1, 1)
 
-        self.cbs3 = CBS(c1, c1, 3, 2, 1)
-
-        self.cbs2 = CBS(3 * c1, c2, 1, 1)
-
-    def forward(self, x):
-
-        x1 = self.spd(x)
-        x1 = self.cbs(x1)
-
-        x2 = self.cbs3(x)
-
-        x3 = torch.nn.functional.avg_pool2d(x, 2, 1, 0, False, True)
-        x3 = torch.nn.functional.max_pool2d(x3, 3, 2, 1)
-        x3 = self.cbs1(x3)
-
-        x = torch.cat([x1, x2, x3], dim=1)
-
-        x = self.cbs2(x)
-
-        return x
+# The key code of this module will be publicly released after the paper is published.

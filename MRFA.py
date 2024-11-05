2927f37c24b3109d4836c370c9cc630796c0049b
ZikaiChen <149089196+Zikai-Chen@users.noreply.github.com>
2024-11-05 13:14:15 +0800

    Update MRFA.py

--- MRFA.py
+++ MRFA.py
@@ -2,51 +2,10 @@ from torch import nn
 from einops import rearrange
 import torch
 
-class MRFAConv(nn.Module):
-    def __init__(self, in_channel, out_channel, kernel_size=3, stride=1):
-        super().__init__()
-        self.kernel_size = kernel_size
-
-        self.get_weight = nn.Sequential(nn.AvgPool2d(kernel_size=kernel_size, padding=kernel_size // 2, stride=stride),
-                                        nn.Conv2d(in_channel, in_channel * (kernel_size ** 2), kernel_size=1,
-                                                  groups=in_channel, bias=False))
 
 
-        self.channel_weight = nn.Sequential(nn.AvgPool2d(kernel_size=kernel_size, padding=kernel_size // 2, stride=stride),
-                                        nn.Conv2d(in_channel, in_channel * (kernel_size ** 2), kernel_size=1,
-                                                  groups=in_channel, bias=False))
+# The key code of this module will be publicly released after the paper is published.
 
-
-        self.generate_feature = nn.Sequential(
-            nn.Conv2d(in_channel, in_channel * (kernel_size ** 2), kernel_size=kernel_size, padding=kernel_size // 2,
-                      stride=stride, groups=in_channel, bias=False),
-            nn.BatchNorm2d(in_channel * (kernel_size ** 2)),
-            nn.ReLU())
-
-        self.conv = Conv(in_channel, out_channel, k=kernel_size, s=kernel_size, p=0)
-
-    def forward(self, x):
-        b, c = x.shape[0:2]
-        weight = self.get_weight(x)
-        h, w = weight.shape[2:]
-        weighted = weight.view(b, c, self.kernel_size ** 2, h, w).softmax(2)  # b c*kernel**2,h,w ->  b c k**2 h w
-        feature = self.generate_feature(x).view(b, c, self.kernel_size ** 2, h,
-                                                w)  # b c*kernel**2,h,w ->  b c k**2 h w
-
-
-        channel_weight = self.channel_weight(x)
-        channel_weight = channel_weight.view(b, c, self.kernel_size ** 2, h, w)
-        channel_weight = channel_weight.permute(0, 2, 1, 3, 4)
-        channel_weight = channel_weight * channel_weight.softmax(2)
-        channel_weight = torch.sigmoid(channel_weight)
-        channel_weight = channel_weight.permute(0, 2, 1, 3, 4)
-
-
-        weighted_data = feature * weighted * channel_weight
-        conv_data = rearrange(weighted_data, 'b c (n1 n2) h w -> b c (h n1) (w n2)', n1=self.kernel_size,
-                              # b c k**2 h w ->  b c h*k w*k
-                              n2=self.kernel_size)
-        return self.conv(conv_data)
 
 
 def autopad(k, p=None, d=1):  # kernel, padding, dilation

69c64c5dc4016aec0d2439c6bb202cc26f661e7d
ZikaiChen <149089196+Zikai-Chen@users.noreply.github.com>
2024-11-27 22:21:37 +0800

    Update LMSA.py

--- nn/mymodules/LMSA.py
+++ nn/mymodules/LMSA.py
@@ -91,7 +91,7 @@ class CoordAtt(nn.Module):
 
 
 
-class Bottleneck_LSMA(nn.Module):
+class Bottleneck_LMSA(nn.Module):
     """Standard bottleneck."""
 
     def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):
@@ -100,8 +100,8 @@ class Bottleneck_LSMA(nn.Module):
         """
         super().__init__()
         c_ = int(c2 * e)  # hidden channels
-        self.cv1 = LSMA(c1, c_)
-        self.cv2 = LSMA(c_, c2)
+        self.cv1 = LMSA(c1, c_)
+        self.cv2 = LMSA(c_, c2)
         self.add = shortcut and c1 == c2
 
     def forward(self, x):
@@ -109,7 +109,7 @@ class Bottleneck_LSMA(nn.Module):
         return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
 
 
-class C2f_LSMA(nn.Module):
+class C2f_LMSA(nn.Module):
     """Faster Implementation of CSP Bottleneck with 2 convolutions."""
 
     def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
@@ -121,7 +121,7 @@ class C2f_LSMA(nn.Module):
         self.cv1 = Conv(c1, 2 * self.c, 1, 1)
         self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
         self.m = nn.ModuleList(
-            Bottleneck_LSMA(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+            Bottleneck_LMSA(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
 
     def forward(self, x):
         """Forward pass through C2f layer."""
